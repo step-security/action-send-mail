9b6cb4da9617e2ceac04037f894ca5df5d30d2ba
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-01-21 20:57:34 +0530

    updated stream

--- main.js
+++ main.js
@@ -43,8 +43,28 @@ function sleep(ms) {
     });
 }
 
+
+async function validateSubscription() {
+    const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
+
+    try {
+      await axios.get(API_URL, {timeout: 3000});
+    } catch (error) {
+      if (error.response) {
+        console.error(
+          'Subscription is not valid. Reach out to support@stepsecurity.io'
+        );
+        process.exit(1);
+      } else {
+        core.info('Timeout or API not reachable. Continuing to next step.');
+      }
+    }
+  }
+
 async function main() {
     try {
+        await validateSubscription();
+        
         let serverAddress = core.getInput("server_address")
         let serverPort = core.getInput("server_port")
         let secure = core.getInput("secure")
